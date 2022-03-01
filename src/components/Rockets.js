0ebf67f726b59e53d9fdf554a52aea8a938af84c
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-01 20:24:50 +0200

    Dispatch corresponding actions on click

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,8 +1,10 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { reserveRocket, cancelReservation } from '../redux/rockets/rockets';
 import './Rockets.css';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rocketsReducer);
+  const dispatch = useDispatch();
 
   return (
     <section className="rockets">
@@ -11,8 +13,14 @@ const Rockets = () => {
           <img className="img" src={item.flickr_images} alt="rocket" />
           <div className="rocket-info">
             <h2>{item.rocket_name}</h2>
-            <p>{rockets[0].description}</p>
-            <button className="btn btn-reserve" type="button">Reserve Rocket</button>
+            {item.reserved ? (
+              <p>
+                <span className="span">Reserved</span>
+                {rockets[0].description}
+              </p>
+            ) : <p>{rockets[0].description}</p>}
+            { (item.reserved) ? <button className="btn btn-cancel-reservation" type="button" onClick={() => dispatch(cancelReservation(item.id))}>Cancel Reservation</button>
+              : <button className="btn btn-reserve" type="button" onClick={() => dispatch(reserveRocket(item.id))}>Reserve Rocket</button>}
           </div>
         </div>
       ))}
