1c3123c108af22a673644645d7f96000ac049be0
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-01 18:57:07 +0200

    Fetch rockets data

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,7 +2,7 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rocketsReducer from './rockets/rockets';
+import rocketsReducer, { fetchData } from './rockets/rockets';
 import missionsReducer from './missions/missions';
 
 const reducer = combineReducers({
@@ -13,4 +13,5 @@ const store = createStore(
   reducer,
   composeWithDevTools(applyMiddleware(thunk, logger)),
 );
+store.dispatch(fetchData());
 export default store;
