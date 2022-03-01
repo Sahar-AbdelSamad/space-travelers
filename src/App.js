c970bb514587a0575c2dd3acb95968afe93c4e26
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-01 18:19:46 +0200

    Import BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router, Routes, Route,
+} from 'react-router-dom';
 import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Profile from './components/MyProfile';
