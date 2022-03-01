ed308fd5c82b3587da23e3b93fee1fc9c708253d
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-02 01:28:54 +0200

    Style the project and make it responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,91 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { FiMenu } from 'react-icons/fi';
+import { MdClose } from 'react-icons/md';
 import './Header.css';
 
-const Header = () => (
-  <header className="header">
-    <div className="fantastic-logo" />
-    <Link to="/" style={{ textDecoration: 'none' }}><h1 className="logo">Space Travelers&apos; Hub</h1></Link>
-    <nav className="nav">
-      <ul className="navBar">
-        <li className="nav-link books">
-          <Link to="/" style={{ textDecoration: 'none' }}>ROCKETS</Link>
-        </li>
-        <li className="nav-link categories">
-          <Link to="missions" style={{ textDecoration: 'none' }}>MISSIONS</Link>
-        </li>
-        <li className="nav-link categories">
-          <Link to="myProfile" style={{ textDecoration: 'none' }}>My Profile</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+  const closeMenu = () => {
+    setNavbarOpen(false);
+  };
+  return (
+    <header className="header">
+      <NavLink to="/">
+        <div className="fantastic-logo" />
+        <h1 className="logo">Space Travelers&apos; Hub</h1>
+      </NavLink>
+      <nav className="nav mobile-nav">
+        <button type="button" onClick={handleToggle}>
+          {navbarOpen ? (
+            <MdClose style={{ color: '#fff', width: '40px', height: '40px' }} />
+          ) : (
+            <FiMenu style={{ color: '#7b7b7b', width: '40px', height: '40px' }} />
+          )}
+        </button>
+        <ul className={`menuNav ${navbarOpen ? ' showMenu navBar' : 'navBar'}`}>
+          <li className="nav-link">
+            <NavLink
+              to="/"
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li className="nav-link missions">
+            <NavLink
+              to="missions"
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li className="nav-link">
+            <NavLink
+              to="myProfile"
+              activeClassName="active-link"
+              onClick={() => closeMenu()}
+              exact
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <nav className="nav desktop-nav">
+        <ul className="navBar">
+          <li className="nav-link">
+            <NavLink
+              to="/"
+            >
+              Rockets
+            </NavLink>
+          </li>
+          <li className="nav-link missions">
+            <NavLink
+              to="missions"
+            >
+              Missions
+            </NavLink>
+          </li>
+          <li className="nav-link">
+            <NavLink
+              to="myProfile"
+            >
+              My Profile
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
