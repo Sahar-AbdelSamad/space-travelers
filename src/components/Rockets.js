61ac2b968d7ca77dcf0c950dfa1460ff8e018200
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-03-01 19:38:04 +0200

    Render lists of rockets in Rockets component

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,5 +1,23 @@
-const Rockets = () => (
-  <h1>Rockets</h1>
-);
+import { useSelector } from 'react-redux';
+import './Rockets.css';
+
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+
+  return (
+    <section className="rockets">
+      {rockets.map((item) => (
+        <div className="flex" key={item.id}>
+          <img className="img" src={item.flickr_images} alt="rocket" />
+          <div className="rocket-info">
+            <h2>{item.rocket_name}</h2>
+            <p>{rockets[0].description}</p>
+            <button className="btn btn-reserve" type="button">Reserve Rocket</button>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+};
 
 export default Rockets;
