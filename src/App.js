4c3e62b38ec49003a85f3915d20129bccb7f5ec4
oyelaking9 <oyelakinr37@gmail.com>
2022-03-02 11:18:15 -0800

    Create mission action

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Rockets from './components/Rockets';
 import Missions from './components/Missions';
 import Profile from './components/MyProfile';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/MissionsTable.js
+++ src/components/MissionsTable.js
@@ -35,5 +35,4 @@ const MissionsTable = () => {
     </div>
   );
 };
-
 export default MissionsTable;
