a0f8dc42e5db754a2384d05e206b88a06c763358
oyelaking9 <oyelakinr37@gmail.com>
2022-03-03 02:47:19 -0800

    Modify my profile js file

--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import ReservedMissionsList from './ReservedMissionList';
 import './MyProfile.css';
 
 const Profile = () => {
@@ -7,7 +8,10 @@ const Profile = () => {
 
   return (
     <>
-      <section className="mission">Profile</section>
+      <section className="mission">
+      <h2>My Missions</h2>
+        <ReservedMissionsList />
+      </section>
       <section className="rockets">
         <h1>My Rockets</h1>
         <table>
