44cb16427bcf7b4f414de6a10c7466a9c644df0f
oyelaking9 <oyelakinr37@gmail.com>
2022-03-03 02:42:01 -0800

    Add reserve mission list js file

---
+++ src/components/ReservedMissionList.js
@@ -0,0 +1,31 @@
+import { useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import ReservedMissions from './ReservedMissions';
+
+const ReservedMissionsList = () => {
+  const missions = useSelector((state) => state.missions);
+  const reserved = missions.data.filter((missions) => missions.reserved);
+
+  return (
+    <>
+      { reserved.length === 0
+      && <p >&emsp; Join a Mission First</p>}
+      <table className="table table-bordered rounded">
+        <tbody>
+          {
+          reserved.map((mission) => (
+            <ReservedMissions
+              key={uuidv4()}
+              missionName={mission.mission_name}
+              missionId={mission.mission_id}
+              wikipedia={mission.wikipedia}
+            />
+          ))
+        }
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default ReservedMissionsList;
